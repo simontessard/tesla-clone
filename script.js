fb032eff9e2e647c0da24d2a5d05076ae4b171d1
simon <simon.tessard@gmail.com>
2024-03-07 14:37:17 +0100

    Text animation

--- script.js
+++ script.js
@@ -14,14 +14,26 @@ panels.forEach((panel, i) => {
         pin: true,
         pinSpacing: false
     });
+
+    let textCard = panel.querySelector(".js-text-card");
+    if (textCard) {
+        gsap.from(textCard, {
+            scrollTrigger: {
+                trigger: panel, // L'animation se déclenche en fonction du panel
+                start: "top center", // L'animation se déclenche lorsque le haut du panel atteint le milieu de la fenêtre de visualisation
+            },
+            autoAlpha: 0,
+            duration: 2,
+        });
+    }
 });
 
 ScrollTrigger.create({
     snap: {
         snapTo: (progress, self) => {
-            let panelStarts = tops.map(st => st.start), // an Array of all the starting scroll positions. We do this on each scroll to make sure it's totally responsive. Starting positions may change when the user resizes the viewport
-                snapScroll = gsap.utils.snap(panelStarts, self.scroll()); // find the closest one
-            return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll); // snapping requires a progress value, so convert the scroll position into a normalized progress value between 0 and 1
+            let panelStarts = tops.map(st => st.start),
+                snapScroll = gsap.utils.snap(panelStarts, self.scroll());
+            return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll);
         },
         duration: 0.3
     }
